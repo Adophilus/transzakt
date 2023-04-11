a8072c81097deb52b5a8d72ec57137d3ded539bd
Adophilus <uchenna19of@gmail.com>
2023-04-11 18:47:05 +0100

    added the has uuid trait to the transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $fillable = [
         'user_id',
         'type',
