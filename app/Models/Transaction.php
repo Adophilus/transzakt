7923c67eb0219dbe1396fad71607c0996371418e
Adophilus <uchenna19of@gmail.com>
2023-04-12 06:46:55 +0100

    made the transaction model searchable by scout

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Laravel\Scout\Searchable;
 
 class Transaction extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, Searchable;
+
     protected $fillable = [
         'user_id',
         'type',
@@ -20,5 +22,10 @@ class Transaction extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function toSearchableArray(): array
+    {
+        return ['id' => $this->id, 'type' => $this->type, 'amount' => $this->amount, 'status' => $this->status, 'user_id' => $this->user_id, 'created_at' => $this->created_at, 'updated_at' => $this->updated_at];
     }
 }
